4ab09b2bbb63ce0a248d5746409d9356327f32a2
Ayushya Saxena <ayushyasaxena170@gmail.com>
2023-01-06 15:17:46 +0530

    Time: 1 ms (55.45%), Space: 45.1 MB (56.64%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,101 +10,24 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if(head == null || head.next == null){
-            return head;
-        }
-        ListNode ch = head;
-        ListNode cur = ch;
-        ListNode p = null;
-        
-        int n = count(head) / k;
-        System.out.println(n);
-        
-        for(int i = 0; i < n; i++){
-
-            int nk = k;
-            if(ch == head){
-            
-                while(cur != null && cur.next != null && nk > 1){
-                
-                    cur = cur.next;
-                    nk--;
-                }
-                
-                ListNode f = cur.next;
-                cur.next = null;
-                ch = reverseList(ch);
-                cur = ch;
-                
-                while(cur != null && cur.next != null){
-                    cur = cur.next;
-                }
-
-                cur.next = f;
-                head = ch;
-                ch = cur.next;
-                p = cur;
-                cur = ch;
-                
-            }else{
-                
-                while(cur != null && cur.next != null && nk > 1){
-
-                    cur = cur.next;
-                    nk--;
-                }
-                
-                ListNode f = cur.next;
-                cur.next = null;
-                p.next = null;
-                ch = reverseList(ch);
-                p.next = ch;
-                cur = ch;
-                
-                while(cur != null && cur.next != null){
-                
-                    cur = cur.next;
-                }
-
-                cur.next = f;
-                ch = cur.next;
-                p = cur;
-                cur = ch;
-            }
-        }
-        
-        
-        return head;
-    }
-    
-    public int count(ListNode head){
         ListNode curr = head;
-        int c = 0;
-        while(curr != null){
+        int count = 0;
+        while (curr != null && count != k) { // find the k+1 node
             curr = curr.next;
-            c++;
+            count++;
         }
-        System.out.println(c+1);
-        return c++;
-    }
-    
-    
-    
-    public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) return head;
-
-        ListNode c = head;
-        ListNode p = null;
-        ListNode f = null;
-
-        while(c != null){
-            f = c.next;
-            c.next = p;
-
-            p = c;
-            c= f;
-        }     
-        
-        return p;
+        if (count == k) { // if k+1 node is found
+            curr = reverseKGroup(curr, k); // reverse list with k+1 node as head
+            // head - head-pointer to direct part, 
+            // curr - head-pointer to reversed part;
+            while (count-- > 0) { // reverse current k-group: 
+                ListNode tmp = head.next; // tmp - next head in direct part
+                head.next = curr; // preappending "direct" head to the reversed list 
+                curr = head; // move head of reversed part to a new node
+                head = tmp; // move "direct" head to the next node in direct part
+            }
+            head = curr;
+        }
+        return head;
     }
 }
